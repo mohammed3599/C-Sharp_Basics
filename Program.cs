8533d6312adf6eefd8424a1d3e0ce81ff7e59e29
mohammed3599 <kataloni1999@gmail.com>
2023-05-30 11:22:02 +0400

    Add user information

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Security;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Name: Mohammed");
+        Console.WriteLine("Father Name: Abdullah");
+        Console.WriteLine("DOB: 1999-5-3");
+        Console.WriteLine("Email Address: kataloni1999@gmail.com");
+        Console.WriteLine(" GitHub Username: mohammed3599");
+    }
+}
